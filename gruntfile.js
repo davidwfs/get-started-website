8c370e8452fc70cd4fe8f362b45829cef347e9f3
David Washington <davidwashington833@gmail.com>
2018-06-24 14:15:48 -0300

    Configurando o comando npm start

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,52 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    './src/assets/css/main.css': './src/assets/scss/main.scss'
+                }
+            }
+        },
+        php: {
+            dev: {
+                options: {
+                    port: 8000,
+                    base: './src/'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ['./src/**/*.{php,html}', './src/assets/**/*.{png,jpg,gif,svg,css,js}']
+                },
+                options: {
+                    proxy: '127.0.0.1:8000',
+                    port: 8000,
+                    open: false,
+                    watchTask: true
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass']
+            },
+            php: {
+                files: ['**/*.{php,html,css}']
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-php');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('server', ['sass', 'php', 'browserSync', 'watch']);
 }
